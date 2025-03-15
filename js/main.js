8f62aed488c31beea1df3e5134c15ef7fcaadcdf
MoazMorsh <116154776+MoazMorsh@users.noreply.github.com>
2025-03-15 15:32:56 +0200

    edits2

--- js/main.js
+++ js/main.js
@@ -14,43 +14,43 @@ let newLaunchesList = [
     {
         name: "Kotex Overnight Period Panties - Medium / Large Size, Pack Of 2 Panties",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/W66447/kotex-overnight-period-panties-medium-large-size-pack-of-2-panties-2-1652778799.jpg?dim=1024x0",
-        mrp: 95,
+        EGP: 95,
         discount: 15,
     },
     {
         name: "Pharmeasy Posture Corrector - Corrects Bad Posture - Eases Neck, Back & Shoulder Pain - Small",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/G63237/pharmeasy-posture-corrector-corrects-bad-posture-eases-neck-back-shoulder-pain-small-2-1656420762.jpg?dim=1024x0",
-        mrp: 999,
+        EGP: 999,
         discount: 50,
     },
     {
         name: "Sugar Pop Nourishing Lip Balm - 02 Cherry",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T31617/sugar-pop-nourishing-lip-balm-02-cherry-2-1653658203.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 149,
+        EGP: 149,
         discount: 5,
     },
     {
         name: "Manforce Epic Desire Super Thin Premium Condoms, Silk Chocolate Flavor, Disposable Pouch (3 Counts)",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/G89746/manforce-epic-desire-super-thin-premium-condoms-silk-chocolate-flavor-disposable-pouch-3-counts-6.1-1652957426.jpg?dim=1024x0",
-        mrp: 80,
+        EGP: 80,
         discount: 15,
     },
     {
         name: "Kellogg'S Froot Loops Original 285g Box",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/E65383/kelloggs-froot-loops-original-285g-box-2-1644845403.jpg?dim=1024x0",
-        mrp: 185,
+        EGP: 185,
         discount: 0,
     },
     {
         name: "Lacto Calamine Aloe Vera Gel With Vitamin E And Glycerin No Parabens, No Sulphates",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/U05418/lacto-calamine-aloe-vera-gel-with-vitamin-e-and-glycerin-no-parabens-no-sulphates-2-1649248955.jpg?dim=1024x0",
-        mrp: 240,
+        EGP: 240,
         discount: 25,
     },
     {
         name: "Lacto Calamine Aloe Vera Gel With Vitamin E And Glycerin No Parabens, No Sulphates",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/U05418/lacto-calamine-aloe-vera-gel-with-vitamin-e-and-glycerin-no-parabens-no-sulphates-2-1649248955.jpg?dim=1024x0",
-        mrp: 240,
+        EGP: 240,
         discount: 25,
     },
 ];
@@ -59,43 +59,43 @@ let trendingProdList = [
     {
         name: "Supradyn Daily Multivitamin Tablets 15s",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T70695/supradyn-daily-multivitamin-tablets-15s-1-1641793954.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 54,
+        EGP: 54,
         discount: 5,
     },
     {
         name: "Supradyn Daily Multivitamin Tablets 15s",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T70695/supradyn-daily-multivitamin-tablets-15s-1-1641793954.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 54,
+        EGP: 54,
         discount: 5,
     },
     {
         name: "Everherb Karela Jamun Juice - Helps Maintains Healthy Sugar Levels -Helps In Weight Management - 1l",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/L79986/everherb-karela-jamun-juice-helps-maintains-healthy-sugar-levels-helps-in-weight-management-1l-2-1654234653.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 399,
+        EGP: 399,
         discount: 48,
     },
     {
         name: "Dr Morepen Gluco One Bg 03 Glucometer Test Strips Box Of 50",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I05582/dr-morepen-gluco-one-bg-03-glucometer-test-strips-box-of-50-1-1654168216.jpg?dim=1024x0",
-        mrp: 840,
+        EGP: 840,
         discount: 24,
     },
     {
         name: "Dettol Antiseptic Liquid Bottle Of 550 Ml",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I40695/dettol-antiseptic-liquid-bottle-of-550-ml-2-1656923531.jpg?dim=1024x0",
-        mrp: 194.6,
+        EGP: 194.6,
         discount: 7,
     },
     {
         name: "Liveasy Essentials Copper Bottle - Ayurvedic Health Benefits - Leak Proof Cap - 950ml",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/W00450/liveasy-essentials-copper-bottle-ayurvedic-health-benefits-leak-proof-cap-950ml-1-1641793079.jpg?dim=1024x0",
-        mrp: 1100,
+        EGP: 1100,
         discount: 30,
     },
     {
         name: "Vicks Vaporub 25ml, Relief From Cold, Cough, Headache And Body Pain",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/181135/vicks-vaporub-25ml-relief-from-cold-cough-headache-and-body-pain-2-1654078746.jpg?dim=1024x0",
-        mrp: 85,
+        EGP: 85,
         discount: 11,
     },
 ];
@@ -105,7 +105,7 @@ let freqBookedLabTestList = [
         discount: 60,
         name: "Post Pradndial Blood Sugar (PPBS)",
         description: "Measure your blood sugar levels after last meal",
-        mrp: 500,
+        EGP: 500,
         price: 199,
         imgUrl: "https://s3.ap-south-1.amazonaws.com/pe-neon-public/diagnostics/production/recommendationWidget/b4682c3fb22d39479c6d7a4a481e5ae5.png?dim=96x0",
     },
@@ -114,7 +114,7 @@ let freqBookedLabTestList = [
         name: "Comprehensive Full Body Checkup with Vitamin D & B12",
         description:
             "Measure Vitamin D & B12 levels and other essential parameters",
-        mrp: 4199,
+        EGP: 4199,
         price: 1499,
         imgUrl: "https://s3.ap-south-1.amazonaws.com/pe-neon-public/diagnostics/production/recommendationWidget/9fc5a0ab225a3ca2bc1702149e07f311.png?dim=96x0",
     },
@@ -122,7 +122,7 @@ let freqBookedLabTestList = [
         discount: 71,
         name: "Random Blood Sugar (RBS)",
         description: "Testing of the blood sugar level at any time of the day",
-        mrp: 350,
+        EGP: 350,
         price: 99,
         imgUrl: "https://s3.ap-south-1.amazonaws.com/pe-neon-public/diagnostics/production/recommendationWidget/1d842b1450b53455922a5b6de3a3a980.png?dim=96x0",
     },
@@ -131,7 +131,7 @@ let freqBookedLabTestList = [
         name: "",
         description:
             "To assess the renal function for early detection of any kidney disease",
-        mrp: "",
+        EGP: "",
         price: 99,
         imgUrl: "https://s3.ap-south-1.amazonaws.com/pe-neon-public/diagnostics/production/recommendationWidget/d0c1ca33ee2637239dfed1645ed02aa8.png?dim=96x0",
     },
@@ -168,38 +168,38 @@ let wellnessEssentialsList = [
     {
         name: "Evion 400mg Strip of 10 Capsules",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/071710/evion-400mg-strip-of-10-capsules-2-1656662057.jpg?dim=1024x0",
-        mrp: 35,
+        EGP: 35,
         discount: 10,
     },
 
     {
         name: "Neurobin Forte Tablet 30's",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I09432/neurobion-forte-tablet-30s-2-1656661904.jpg?dim=1024x0",
-        mrp: 34.7,
+        EGP: 34.7,
         discount: 10,
     },
     {
         name: "Gas O Fast Active Jeera Axtacid Sachet Of 5 G",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I34550/gas-o-fast-active-jeera-antacid-sachet-of-5-g-2-1654166983.jpg?dim=1024x0",
-        mrp: 10,
+        EGP: 10,
         discount: 15,
     },
     {
         name: "Pharmeasy Multivitamin Multimineral - Immunity Booster - Complete Nutrition - Bottle Of 60",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T60720/pharmeasy-multivitamin-multimineral-immunity-booster-complete-nutrition-bottle-of-60-2-1658316798.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 54,
+        EGP: 54,
         discount: 15,
     },
     {
         name: "Electral Powder 21.80gm",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/065285/electral-powder-2180gm-2-1653986181.jpg?dim=1024x0",
-        mrp: 21.05,
+        EGP: 21.05,
         discount: 15,
     },
     {
         name: "Supradyn Daily Multivitamin Tablets 15s",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T70695/supradyn-daily-multivitamin-tablets-15s-1-1641793954.jpg?dim=1024x0",
-        mrp: 54,
+        EGP: 54,
         discount: 5,
     },
 ];
@@ -290,43 +290,43 @@ let dealsOfTheDayList = [
     {
         name: "Sirona Feminine Cramp Relief Patch Packet Of 5 'S",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I48459/sirona-feminine-cramp-relief-patch-packet-of-5-s-2-1641788205.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 240,
+        EGP: 240,
         discount: 10,
     },
     {
         name: "Liveasy Wellness Apple Cider Vinegar With Mother -Improves Metabolism- 500ml Bottle",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/J51815/liveasy-wellness-apple-cider-vinegar-with-mother-improves-metabolism-500ml-bottle-2-1654165611.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 640,
+        EGP: 640,
         discount: 65,
     },
     {
         name: "Revital H Women Multivitamin With Calcium, Zinc, Ginseng For Immunity, Strong Bones & Energy 30 Tablets",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I00596/revital-h-women-multivitamin-with-calcium-zinc-ginseng-for-immunity-strong-bones-energy-30-tablets-1-1654168794.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 345,
+        EGP: 345,
         discount: 15,
     },
     {
         name: "Volini Pain Relief Gel Tube Of 75 G",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/183157/volini-pain-relief-gel-tube-of-75-g-1-1654078754.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 245,
+        EGP: 245,
         discount: 15,
     },
     {
         name: "Evion 400mg Strip of 10 Capsules",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/071710/evion-400mg-strip-of-10-capsules-2-1656662057.jpg?dim=1024x0",
-        mrp: 35,
+        EGP: 35,
         discount: 10,
     },
     {
         name: "Livogen Z Captab 15'S",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I32380/livogen-z-captab-15s-2-1656662098.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 70.4,
+        EGP: 70.4,
         discount: 10,
     },
     {
         name: "Volini Pain Relief Gel Tube Of 75 G",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/183157/volini-pain-relief-gel-tube-of-75-g-1-1654078754.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 245,
+        EGP: 245,
         discount: 15,
     },
     
@@ -336,44 +336,44 @@ let spotlightList = [
     {
         name: "Evion 400mg Strip of 10 Capsules",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/071710/evion-400mg-strip-of-10-capsules-2-1656662057.jpg?dim=1024x0",
-        mrp: 35,
+        EGP: 35,
         discount: 10,
     },
     {
         name: "Evion 400mg Strip of 10 Capsules",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/071710/evion-400mg-strip-of-10-capsules-2-1656662057.jpg?dim=1024x0",
-        mrp: 35,
+        EGP: 35,
         discount: 10,
     },
 
     {
         name: "Neurobin Forte Tablet 30's",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I09432/neurobion-forte-tablet-30s-2-1656661904.jpg?dim=1024x0",
-        mrp: 34.7,
+        EGP: 34.7,
         discount: 10,
     },
     {
         name: "Gas O Fast Active Jeera Axtacid Sachet Of 5 G",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/I34550/gas-o-fast-active-jeera-antacid-sachet-of-5-g-2-1654166983.jpg?dim=1024x0",
-        mrp: 10,
+        EGP: 10,
         discount: 15,
     },
     {
         name: "Pharmeasy Multivitamin Multimineral - Immunity Booster - Complete Nutrition - Bottle Of 60",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T60720/pharmeasy-multivitamin-multimineral-immunity-booster-complete-nutrition-bottle-of-60-2-1658316798.jpg?dim=320x320&dpr=1&q=100",
-        mrp: 54,
+        EGP: 54,
         discount: 15,
     },
     {
         name: "Electral Powder 21.80gm",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/065285/electral-powder-2180gm-2-1653986181.jpg?dim=1024x0",
-        mrp: 21.05,
+        EGP: 21.05,
         discount: 15,
     },
     {
         name: "Supradyn Daily Multivitamin Tablets 15s",
         imgUrl: "https://cdn01.pharmeasy.in/dam/products_otc/T70695/supradyn-daily-multivitamin-tablets-15s-1-1641793954.jpg?dim=1024x0",
-        mrp: 54,
+        EGP: 54,
         discount: 5,
     },
 ];
@@ -425,39 +425,39 @@ let healthArticleList = [
 
 let customerReviewList = [
     {
-        name: "Rajarshi Sarkar",
+        name: "The Egyptian Pharaoh",
         date: "April 22, 2022",
         review: "The app is really wonderful. Being a Product Manager myself, I would say that the User experience (UI/UX) of the app is top notch (easy to use, simple and convenient). Coming to services and delivery, I would say Pharmeasy is doing a tremendous job even during this unprecedented pandemic situation.",
     },
     {
-        name: "Darpan Dholakia",
-        date: "April 23, 2022",
+        name: "Manal Elkhayat",
+        date: "April 10, 2022",
         review: "Best service and app amongst all available. I have been using it for more than 3 years, and even during the pandemic, they have kept their standards high and are delivering the order within 24 hours. Keep up the good work.",
     },
     {
-        name: "Lipi Chaudhuri",
+        name: "Ahmed Khaled",
         date: "April 15, 2022",
-        review: "This app is a game changer for me. I am unable to go out always to buy medicinal products. Pharmeasy gives me the last liberty to shop essential healthcare products from home. The app is very user friendly and me being an elderly person do not find any difficulty in using it. They deliver well in time. Thanks😊",
+        review: "This app is a game changer for me. I am unable to go out always to buy medicinal products. MediTrack gives me the last liberty to shop essential healthcare products from home. The app is very user friendly and me being an elderly person do not find any difficulty in using it. They deliver well in time. Thanks😊",
     },
     {
-        name: "Tirthankar Das Purkayastha",
+        name: "Mansour Attia",
         date: "April 23, 2022",
         review: "Very good app. Would recommend it to everyone requiring fast and efficient delivery of medicinal products at the doorstep.",
     },
     {
-        name: "Debanjan Roy",
+        name: "Lina Sayed",
         date: "April 18, 2022",
-        review: "Excellent experience. Pharmeasy has not let it's customers down during lockdown. Thanks team. Great job. The application is also very smooth. And does its job well with an attractive UI and easy to use features. Good job developer.",
+        review: "Excellent experience. MediTrack has not let it's customers down during lockdown. Thanks team. Great job. The application is also very smooth. And does its job well with an attractive UI and easy to use features. Good job developer.",
     },
     {
-        name: "Rohini Sarkar",
+        name: "Mohamed Ahmed",
         date: "April 21, 2022",
         review: "Very helpful and friendly app in terms of usability, customer support & money saving from the point of medical necessity of every person.",
     },
 ];
 
-let bannerContainer = document.querySelector(".banner");
-createOfferList(bannerList, bannerContainer)
+// let bannerContainer = document.querySelector(".banner");
+// createOfferList(bannerList, bannerContainer)
 
 let newLaunchesContainer = document.querySelector(".new-launches > .product-cards");
 createProductList(newLaunchesList, newLaunchesContainer)
@@ -468,8 +468,8 @@ createProductList(trendingProdList, trendingContainer)
 let freqBookedLabTestContainer = document.querySelector(" .freq-booked-lab-tests > .product-banner");
 createProductBannerList(freqBookedLabTestList, freqBookedLabTestContainer )
 
-let paymentOfferContainer = document.querySelector(".payment-offers > div");
-createOfferList(paymentOfferList, paymentOfferContainer);
+// let paymentOfferContainer = document.querySelector(".payment-offers > div");
+// createOfferList(paymentOfferList, paymentOfferContainer);
 
 let wellnessEssentialsContainer = document.querySelector(".wellness-essentials > .product-cards2");
 createProductList(wellnessEssentialsList, wellnessEssentialsContainer);
@@ -477,8 +477,8 @@ createProductList(wellnessEssentialsList, wellnessEssentialsContainer);
 let featuredBrandContainer = document.querySelector(".featured-brands > .brand-promo");
 createFeatureBrandList(featuredBrandList, featuredBrandContainer);
 
-let featuredBrandOfferContainer = document.querySelector(".featured-brands > .brand-offer");
-createOfferList(featuredBrandOfferList, featuredBrandOfferContainer);
+// let featuredBrandOfferContainer = document.querySelector(".featured-brands > .brand-offer");
+// createOfferList(featuredBrandOfferList, featuredBrandOfferContainer);
 
 let dealsOfTheDayContainer = document.querySelector(".deals-of-the-day > .product-cards");
 createProductList(dealsOfTheDayList, dealsOfTheDayContainer);
@@ -489,8 +489,8 @@ createProductList(spotlightList, spotlightContainer);
 let spotlightPromoContainer = document.querySelector(".spotlight > .product-promo");
 createOfferList(productPromo, spotlightPromoContainer);
 
-let healthArticleContainer = document.querySelector(".health-articles > div");
-createHealthArticleList(healthArticleList, healthArticleContainer);
+// let healthArticleContainer = document.querySelector(".health-articles > div");
+// createHealthArticleList(healthArticleList, healthArticleContainer);
 
 let customerReviewContainer = document.querySelector(".customer-review > .reviews");
 createCustomerReview(customerReviewList, customerReviewContainer);
@@ -540,7 +540,7 @@ function createProductBannerList(list, container) {
         
         if (typeof el.discount == "number") {
             discount.innerText = el.discount = "% OFF";
-            price.innerHTML = "<span>£" + el.mrp + "</span><br>" + "£" + el.price;
+            price.innerHTML = "<span>£" + el.EGP + "</span><br>" + "£" + el.price;
         } else {
             discount.innerText = el.discount;
             discount.setAttribute("class", "no-discount");
@@ -561,17 +561,17 @@ function createProductList(list, container){
         let div2 = document.createElement("div");
         let img = document.createElement("img");
         let name = document.createElement("h4");
-        let mrp = document.createElement("p");
+        let EGP = document.createElement("p");
         let price = document.createElement("h4");
 
         img.setAttribute("src", el.imgUrl);
         name.innerText = el.name;
         name.setAttribute("class", "name");
-        mrp.innerHTML = "MRP <span>£" + el.mrp + "</span>";
-        let p = (el.mrp / 100) * (100 - el.discount);
+        EGP.innerHTML = "EGP <span>£" + el.EGP + "</span>";
+        let p = (el.EGP / 100) * (100 - el.discount);
         p = (Math.round(p * 100) / 100).toFixed(2);
         price.innerHTML = "£" + p +  "<span>" + el.discount + "% OFF";
-        div2.append(name, mrp, price);
+        div2.append(name, EGP, price);
         div.append(img, div2);
         container.append(div);
     })
